01ca808e5321cfe427ab3c555bb9462e7c5d57d5
Peggy Premru <ppremru@redhat.com>
2022-11-30 12:04:25 -0500

    two

---
+++ batman/bat_to_tackle.py
@@ -0,0 +1,13 @@
+''' batman module '''
+
+def convert():
+    print('creates output file')
+
+
+def dry_run():
+    print('view only')
+
+
+if __name__ == "__main__":
+    """This runs when you execute 'python3 batman/bat_to_tackle.py'"""
+    dry_run()
